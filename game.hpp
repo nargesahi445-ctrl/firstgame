14f933830e72d0d00359e258f553911b649b4428
vianaamini <vianaamini.co@gmail.com>
2026-06-13 18:22:21 +0330

    add new functions

--- game.hpp
+++ game.hpp
@@ -4,12 +4,12 @@ using namespace std;
 class game {
     team player1;
     team player2;
-
     int round = 1;
-
     bool isOver = false;
 
     public:
+    int getround();
+    bool roundhasended(int);
     void choosehero();
     void playround();
     void startgame();
