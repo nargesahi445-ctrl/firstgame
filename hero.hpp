06c6f935731aed652c48944ab8adee50d42ba52c
nercis <nargesahi445@gmail.com>
2026-06-14 18:31:51 +0330

    handle the error

--- hero.hpp
+++ hero.hpp
@@ -18,15 +18,10 @@ class hero {
 
         virtual ~hero() = default;
 
-<<<<<<< HEAD
         virtual void ability1(hero& enemyTarget, hero& allyTarget ,team& enemyteam, team& myteam) = 0;
-        virtual void ability2(hero& enemyTarget, hero& allyTarget ,team& enemyteam, team& myteam) = 0;
+        virtual void ability2(hero& enemyTarget, hero& allyTarget ,team& enemyteam, team& myteam , game& , int) = 0;
         virtual void specialability(hero& enemyTarget, hero& allyTarget ,team& enemyteam, team& myteam) = 0;
-=======
-        virtual void ability1(hero& enemyTarget, hero& allyTarget) = 0;
-        virtual void ability2(hero& enemyTarget, hero& allyTarget , game& , int ) = 0;
-        virtual void specialability(hero& enemyTarget, hero& allyTarget) = 0;
->>>>>>> ab9a1fb6c20debed0abf52f04e7edf8f24aa2d38
+
 
         void takeDamage(int amount);
         void heal(int amount);
